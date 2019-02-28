9c8181d661ee88e5c0e2c51e13ea8e2ead881aee
Francesco Sciuti <francescosciuti@Francescos-MBP.station>
2019-02-28 18:07:53 +0100

    Update ProjectDetail Component with Tasks FormArray

--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute, Route, ParamMap } from '@angular/router';
 import { ProjectService } from 'src/app/shared/services/project.service';
 import { Observable, Subscription } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'ngptt-project-detail',
@@ -13,11 +13,15 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   .form-control.ng-invalid.ng-touched {
       border: 1px solid red;
   }
+
+  .badge-danger {
+      cursor: pointer;
+  }
   `]
 })
 export class ProjectDetailComponent implements OnInit, OnDestroy {
-  projectSubscription: Subscription;
   @Input() project: Project;
+  projectSubscription: Subscription;
   editProjectForm: FormGroup;
 
   constructor(private route: ActivatedRoute, private projectService: ProjectService) { }
@@ -32,14 +36,45 @@ export class ProjectDetailComponent implements OnInit, OnDestroy {
   }
 
   initForm() {
+    const projectTasks = new FormArray([]);
+    if (this.project.tasks) {
+      for (const task of this.project.tasks) {
+        projectTasks.push(
+          new FormGroup({
+            'name': new FormControl(task.name, Validators.required),
+            'start': new FormControl(new Date(task.start), Validators.required),
+            'duration': new FormControl(task.duration),
+            'isBillable': new FormControl(task.isBillable)
+          })
+        );
+      }
+    }
+
     this.editProjectForm = new FormGroup({
       'code': new FormControl(this.project.code, Validators.required),
       'name': new FormControl(this.project.name, Validators.required),
       'description': new FormControl(this.project.description),
       'priority': new FormControl(this.project.priority, Validators.required),
       'start': new FormControl(this.project.start),
-      'end': new FormControl(this.project.end)
+      'end': new FormControl(this.project.end),
+      'tasks': projectTasks
     });
+  }
+
+  onAddTask() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.editProjectForm.get('tasks')).push(
+      new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'start': new FormControl(null, Validators.required),
+        'duration': new FormControl(null),
+        'isBillable': new FormControl(false)
+      })
+    );
+  }
+
+  onDeleteTask(index: number) {
+    (<FormArray>this.editProjectForm.get('tasks')).removeAt(index);
   }
 
   submitProjectForm() {
