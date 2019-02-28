ef7a69f7adac2c3f4e9ba1a6b0dedcb48d454a06
Francesco Sciuti <francescosciuti@Francescos-MBP.station>
2019-02-28 12:46:23 +0100

    Add FormModule in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProjectListComponent } from './projects/project-list.component';
@@ -13,7 +14,8 @@ import { SearchFilterPipe } from './shared/search-filter.pipe';
     SearchFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
