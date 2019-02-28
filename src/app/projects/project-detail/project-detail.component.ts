572acaf7586ef9b98d9ca372ad4d52222b91c1a2
Francesco Sciuti <francescosciuti@Francescos-MBP.station>
2019-02-28 18:01:16 +0100

    Update ProjectDetail with Edit ReactiveForm (and restore subscription)

--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -1,25 +1,51 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Project } from 'src/app/shared/Project';
 import { ActivatedRoute, Route, ParamMap } from '@angular/router';
 import { ProjectService } from 'src/app/shared/services/project.service';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ngptt-project-detail',
   templateUrl: './project-detail.component.html',
-  styles: []
+  styles: [`
+  .form-control.ng-invalid.ng-touched {
+      border: 1px solid red;
+  }
+  `]
 })
-export class ProjectDetailComponent implements OnInit {
+export class ProjectDetailComponent implements OnInit, OnDestroy {
+  projectSubscription: Subscription;
   @Input() project: Project;
-  project$: Observable<Project>;
+  editProjectForm: FormGroup;
 
   constructor(private route: ActivatedRoute, private projectService: ProjectService) { }
 
   ngOnInit() {
-    this.project$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => this.projectService.get(params.get('code')))
-    );
+    this.projectSubscription = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.projectService.get(params.get('code'))),
+    ).subscribe((project) => {
+      this.project = project;
+      this.initForm();
+    })
   }
 
+  initForm() {
+    this.editProjectForm = new FormGroup({
+      'code': new FormControl(this.project.code, Validators.required),
+      'name': new FormControl(this.project.name, Validators.required),
+      'description': new FormControl(this.project.description),
+      'priority': new FormControl(this.project.priority, Validators.required),
+      'start': new FormControl(this.project.start),
+      'end': new FormControl(this.project.end)
+    });
+  }
+
+  submitProjectForm() {
+  }
+
+  ngOnDestroy() {
+    this.projectSubscription.unsubscribe();
+  }
 }
