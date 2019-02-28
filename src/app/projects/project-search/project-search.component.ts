47aa0c5b58837681c0c3dd7f06c9350948941869
Francesco Sciuti <francescosciuti@Francescos-MacBook-Pro.local>
2019-02-28 13:50:38 +0100

    Update ProjectSearch Component with eventEmitter and ViewChild

--- src/app/projects/project-search/project-search.component.ts
+++ src/app/projects/project-search/project-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'ngptt-project-search',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProjectSearchComponent implements OnInit {
+  @Output() searching = new EventEmitter<{name: string, priority: string, done: string}>();
+  @ViewChild('filterName') name: ElementRef;
+  @ViewChild('filterPriority') priority: ElementRef;
+  @ViewChild('filterDone') done: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  search() {
+    this.searching.emit({
+      name: (<HTMLInputElement>this.name.nativeElement).value,
+      priority: (<HTMLInputElement>this.priority.nativeElement).value,
+      done: (<HTMLInputElement>this.done.nativeElement).value,
+    })
   }
 
 }
