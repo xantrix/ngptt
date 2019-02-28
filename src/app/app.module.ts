65c19c88c4440d1bae87a75c016fa64f66883499
Francesco Sciuti <francescosciuti@Francescos-MBP.station>
2019-02-28 13:04:21 +0100

    Move ProjectList Component files in subdirectory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProjectListComponent } from './projects/project-list.component';
+import { ProjectListComponent } from './projects/project-list/project-list.component';
 import { SearchFilterPipe } from './shared/search-filter.pipe';
 
 
--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Project } from '../shared/Project';
+import { Project } from '../../shared/Project';
 import { NgForm } from '@angular/forms';
 
 @Component({
