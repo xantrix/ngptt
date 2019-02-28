0a3e7a08dbc2805ad899d9401c45b24efe0a62eb
Francesco Sciuti <francescosciuti@Francescos-MBP.station>
2019-02-28 15:48:04 +0100

    Update Cross Routing Paths Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
-import { ProjectListComponent } from './projects/project-list/project-list.component';
+
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { ProjectListComponent } from './projects/project-list/project-list.component';
+import { ProjectInsertComponent } from './projects/project-insert/project-insert.component';
+import { ProjectDetailComponent } from './projects/project-detail/project-detail.component';
 
 const routes: Routes = [
+    { path: 'projects/add', component: ProjectInsertComponent },
+    { path: 'projects/detail', component: ProjectDetailComponent },
     { path: 'projects', component: ProjectListComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
