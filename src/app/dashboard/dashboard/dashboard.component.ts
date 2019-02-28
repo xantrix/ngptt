de960ea73a9c043438ab9d1a4cbfc23ea8d76f4d
Francesco Sciuti <francescosciuti@Francescos-MBP.station>
2019-02-28 15:01:09 +0100

    Add Dashboard Component

---
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngptt-dashboard',
+  templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent implements OnInit {
+  title = "NGPTT";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
